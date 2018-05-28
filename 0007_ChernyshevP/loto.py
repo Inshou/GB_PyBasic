af6d180083c4337db5f38d9c1e73e11405f82920
Inshou <inshou@gmail.com>
2018-05-29 02:17:29 +0400

    7 - Finished

--- 0007_ChernyshevP/loto.py
+++ 0007_ChernyshevP/loto.py
@@ -64,7 +64,7 @@ class Card:
     def newCard(self):
         nums = []
         while len(nums) < 15:
-            value = random.randint(1, 90)
+            value = random.randint(1, 91)
             if value not in nums:
                 nums.append(value)
         self.nums = {a: 0 for a in range(27)}
@@ -85,15 +85,81 @@ class Card:
         for x in range(3):
             line = list(self.nums.values())[x * 9:x * 9 + 9]
             print('{:>3} {:>3} {:>3} {:>3} {:>3} {:>3} {:>3} {:>3} {:>3} '.format(*line))
-        print('------------------------------------')
+        print('------------------------------------\n')
+
+    def check_keg(self, keg):
+        for ind in self.nums:
+            if keg == self.nums[ind]:
+                print(f'Найдено совпадение у игрока {self.user} => {keg}\n')
+                self.nums[ind] = '-'
+
+    def check_win(self):
+        for ind in self.nums:
+            try:
+                int(self.nums[ind])
+                if int(self.nums[ind]) > 0:
+                    return False
+            except ValueError:
+                if str(self.nums[ind]) != '-':
+                    return False
+        return True
+
+class Kegs:
+
+    def __init__(self):
+        try:
+            self.kegs
+        except AttributeError:
+            self.kegs = [a for a in range(1, 91)]
+            random.shuffle(self.kegs)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if len(self.kegs) > 0:
+            return self.kegs.pop()
+        else:
+            raise StopIteration
+
+    def __int__(self):
+        return self
+
+    def show(self):
+        print(self.kegs)
 
 
+def clear():
+    """
+    Функция для очистки консоли перед выводом/вводом данных
+    """
+    print("\n" * 100)
 
 
+kegs = Kegs()
 user_card = Card('Пользователь')
 comp_card = Card('Компьютер')
-user_card.show()
-comp_card.show()
+showen = ''
 
+for keg in kegs:
+    clear()
+    showen = str(keg) + ' ' + showen
+    comp_card.check_keg(keg)
+    user_card.check_keg(keg)
 
+    print(f'Открытые бочонки: {showen}\n')
 
+    comp_card.show()
+    user_card.show()
+
+    if user_card.check_win():
+        print('!!!!!!Вы выиграли!!!!!!')
+        break
+    elif comp_card.check_win():
+        print('!!!!!!Вы проиграли!!!!!!')
+        break
+    else:
+        turn = input('Следующий ход? (Нажмите Enter, \"q\" - для выхода): ')
+        if turn == 'q':
+            print('Спасибо за игру, приходите еще!\n')
+            break
